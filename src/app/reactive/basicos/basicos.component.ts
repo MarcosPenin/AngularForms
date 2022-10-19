477276d26264675770edc1176c89b275b2e46c6c
marcos.gonzalez <marcos.gonzalez@imatia.com>
2022-10-19 12:45:00 +0200

    Reactive forms

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,14 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html'
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent   {
 
-  constructor() { }
+  miFormulario:FormGroup=this.fb.group({
+    nombre: ["RTX 4080ti",[Validators.required,Validators.minLength(3)]],
+    precio:[0,[Validators.required,Validators.min(0)]],
+    existencias:[6,[Validators.required,Validators.min(0)]]
+  })
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(){
+    this.miFormulario.reset({
+      nombre:"RTX 4080ti",
+      precio: 1600,
+  
+    })
   }
+
+
+campoEsValido(campo:string){
+  return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched
+}
+
+guardar(){
+  if(this.miFormulario.invalid){
+   this.miFormulario.markAllAsTouched();
+   return;
+  }
+  console.log(this.miFormulario.value)
+  this.miFormulario.reset()
+}
 
 }
